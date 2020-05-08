3ae6f318f1cb03a09fe7cb0b9590d16c70590052
BlueOwlet <64948785+BlueOwlet@users.noreply.github.com>
2020-05-08 03:23:08 -0500

    Almost done

--- MassPresentation.py
+++ MassPresentation.py
@@ -8,10 +8,11 @@ import openpyxl as xl
 
 levels={
 
-    'INTRO':'INTROTEMPLATE.pptx',
-    'BOOK1':'BOOK1TEMPLATE.pptx',
-    'ADV':'ADVTEMPLATE.pptx',
-
+    'TRNGRAMMAR':'TRNGTEMPLATE.pptx',
+    'TRNCONVERSATION':'TRNCTEMPLATE.pptx',
+    'ADVGRAMMAR':'ADVGTEMPLATE.pptx',
+    'ADVCONVERSATION':'ADVCTEMPLATE.pptx',
+    'test':'test.pptx'
 }
 
 
@@ -32,7 +33,7 @@ row=[row.value for row in rows[0]]
 
 def MassCreatePresentation():
     for i in range(0,len(rows)-1):
-        level=rows[i+1][1].value[0:rows[i+1][1].value.index(' ')]
+        level=rows[i+1][2].value
         template = os.path.abspath(glob.glob(levels[level])[0])
         print('Template Found')
 
@@ -59,18 +60,27 @@ def MassCreatePresentation():
                 lines =file.read()
 
                 while '&lt;&lt;' in lines:
-                    tag =lines[lines.index('&lt;&lt;'):lines.index('&gt;&gt;')+8]
-                    strippedtag=lines[lines.index('&lt;&lt;')+8:lines.index('&gt;&gt;')]
-                    print('indicator found: '+strippedtag)
-
-                    lines= lines.replace(tag,str(rows[i+1][row.index(strippedtag)].value))
-
-                    print('Replaced')
+                    try:
+                        print('pass1')
+                        tag =lines[lines.index('&lt;&lt;'):lines.index('&gt;&gt;')+8]
+                        print(tag)
+                        strippedtag=lines[lines.index('&lt;&lt;')+8:lines.index('&gt;&gt;')]
+                        print('TAG: '+strippedtag)
+                        if row.index(strippedtag)>-1:
+                            print('indicator found: '+strippedtag)
+                            lines= lines.replace(tag,str(rows[i+1][row.index(strippedtag)].value))
+                            print('Replaced with: '+str(rows[i+1][row.index(strippedtag)].value))
+                        else:
+                            print('tag not in excel')
+                    except Exception as e:
+                        # lines= lines.replace(tag,"")
+                        print('error tag not found | tag deleted: '+strippedtag)
+                        break
                 file.seek(0)
                 file.truncate(0)
                 file.write(lines)
-        finalPath='MassProduced/'+str(rows[i+1][0].value)+' '+str(rows[i+1][1].value)+' '+str(rows[i+1][2].value)
-        with ZipFile('MassProduced/'+str(rows[i+1][0].value)+' '+str(rows[i+1][1].value)+' '+str(rows[i+1][2].value)+'.zip','w') as zip:
+        finalPath='MassProduced/'+str(rows[i+1][0].value)+' '+str(rows[i+1][1].value)+' '+str(rows[i+1][3].value)
+        with ZipFile(finalPath+'.zip','w') as zip:
             os.remove(glob.glob('Operating/*.zip')[0])
             # shutil.make_archive('MassProduced/'+str(rows[i+1][0].value)+' '+str(rows[i+1][1].value)+' '+str(rows[i+1][2].value),'zip','Operating/','')
 
@@ -80,8 +90,8 @@ def MassCreatePresentation():
                     print(file)
                     print(folder)
                     filePath = os.path.join(folder,file)
-                    print('FILEPATH IS: ' + filePath)
-                    print('USEFULPATH IS: '+filePath[filePath.index('/')+1:])
+                    # print('FILEPATH IS: ' + filePath)
+                    # print('USEFULPATH IS: '+filePath[filePath.index('/')+1:])
                     zip.write(filePath,filePath[filePath.index('/')+1:])
 
         os.rename(finalPath+'.zip',finalPath+'.pptx')
